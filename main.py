f61507f24f95b19c15529c804c77e05b01377298
AIwanKozlovA <88284457+AIwanKozlovA@users.noreply.github.com>
2022-06-23 20:11:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+import sys  # sys нужен для передачи argv в QApplication
+from ui import Ui_Widget
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+import webbrowser
+app = QtWidgets.QApplication(sys.argv)
+Form = QtWidgets.QWidget()
+ui = Ui_Widget()
+ui.setupUi(Form)
+Form.show()
+eng_ras_nr = "`!@#$%^&qwertyuiop[]asdfghjkl;'zxcvbnm,./"
+#eng_ras_nr = "qwertyuiop[]asdfghjkl;'zxcvbnm,./"
+eng_ras_nr = [char for char in eng_ras_nr]
+rus_ras_nr = 'ё!"№;%:?йцукенгшщзхъфывапролджэячсмитьбю.'
+#rus_ras_nr = "йцукенгшщзхъфывапролджэячсмитьбю."
+rus_ras_nr = [char for char in rus_ras_nr]
+#Верхний регистр
+#eng_ras_vr = [char.upper() for char in eng_ras_nr]
+eng_ras_vr = '~!@#$%^&QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?'
+#rus_ras_vr = [char.upper() for char in rus_ras_nr]
+rus_ras_vr = 'Ё!"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,'
+def from_eng_in_rus():
+    text = ui.textEdit.toPlainText()
+    result = ""
+    for i in text:
+        if i in eng_ras_nr or i in eng_ras_vr:
+            if i in eng_ras_vr:
+                result += rus_ras_vr[eng_ras_vr.index(i)]
+            else:
+                result += rus_ras_nr[eng_ras_nr.index(i)]
+        else:
+            result += i
+    ui.plainTextEdit.setPlainText(result)
+def from_rus_in_en():
+    text = ui.textEdit.toPlainText()
+    result = ""
+    for i in text:
+        if i in rus_ras_nr or i in rus_ras_vr:
+            if i in rus_ras_vr:
+                result += eng_ras_vr[rus_ras_vr.index(i)]
+            else:
+                result += eng_ras_nr[rus_ras_nr.index(i)]
+        else:
+            result += i
+    ui.plainTextEdit.setPlainText(result)
+def edit_reg():
+    text = ui.textEdit.toPlainText()
+    result = ""
+    for i in text:
+        if i.isupper():
+            result += i.lower()
+        else:
+            result += i.upper()
+    ui.plainTextEdit.setPlainText(result)
+def inversion():
+    text = ui.textEdit.toPlainText()
+    result = ""
+    for i in text:
+        if i in rus_ras_vr:
+            result += eng_ras_vr[rus_ras_vr.index(i)]
+        elif i in eng_ras_vr:
+            result += rus_ras_vr[eng_ras_vr.index(i)]
+        elif i in rus_ras_nr:
+            result += eng_ras_nr[rus_ras_nr.index(i)]
+        elif i in eng_ras_nr:
+            result += rus_ras_nr[eng_ras_nr.index(i)]
+        else:
+            result += i
+    ui.plainTextEdit.setPlainText(result)
+def inversion_CL():
+    text = ui.textEdit.toPlainText()
+    result = ""
+    for i in text:
+        if i in rus_ras_vr:
+            result += eng_ras_nr[rus_ras_vr.index(i)]
+        elif i in eng_ras_vr:
+            result += rus_ras_nr[eng_ras_vr.index(i)]
+        elif i in rus_ras_nr:
+            result += eng_ras_vr[rus_ras_nr.index(i)]
+        elif i in eng_ras_nr:
+            result += rus_ras_vr[eng_ras_nr.index(i)]
+        else:
+            result += i
+    ui.plainTextEdit.setPlainText(result)
+def cv():
+    webbrowser.open_new_tab("https://vk.com/ivan___kozlov")
+def queston():
+    msg = QMessageBox()
+    msg.setWindowTitle("Объяснение")
+    msg.setText("Программа 'Переставщик' написана для того, чтобы в случае, если пользователь написал текст, на другой раскладке, то он мог без проблем, перевести текст, на нужную для него раскладку.\n Текст, который вы хотите обработать, нужно вводить в поле ввода 'Входные данные'.\n  Обработанный текст появится в поле 'Результат обработки'\n Программа 'Переставщик', может перевести текст с русской раскладки, на английскую, для этого необходимо ввести текст в верхнее поле ввода и нажать на кнопку 'Перевести на английскую раскладку', результат обработки будет в нижем поле ввода.\n Аналогичным способом, можно перевести с английской на русскою раскладку и изменить регистер для всего текста.\n Если вы, то что должны были написать на русской раскладке, написали на аглийской и, то что должы были написать на английской раскладке написали на русской раскладке, и из-за этого образовалась полная мешанина, то её можно преобразовать в нормальный текст, скопировав текст в поле ввода 'Входные данные' и нажав на кнопку 'Инверсия'. Если вы ещё помимо раскладки перепутали регистер, то на нажмите кнопку 'Инверсия с Caps Lock'\nПриятного пользования;)")
+    #msg.setIcon(QMessageBox.Question)
+    msg.exec_()
+ui.pushButton.clicked.connect(from_eng_in_rus)
+ui.pushButton_2.clicked.connect(from_rus_in_en)
+ui.pushButton_3.clicked.connect(edit_reg)
+#ui.pushButton_5.clicked.setText("Over-write")
+ui.pushButton_4.clicked.connect(queston)
+ui.pushButton_5.clicked.connect(inversion)
+ui.pushButton_6.clicked.connect(inversion_CL)
+ui.pushButton_7.clicked.connect(cv)
+sys.exit(app.exec_())
---
+++ ui.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'form.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        Widget.setObjectName("Widget")
+        Widget.resize(784, 678)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("perestavshic.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Widget.setWindowIcon(icon)
+        self.label = QtWidgets.QLabel(Widget)
+        self.label.setGeometry(QtCore.QRect(20, 100, 161, 51))
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(Widget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 281, 41))
+        self.pushButton.setObjectName("pushButton")
+        self.checkBox_2 = QtWidgets.QCheckBox(Widget)
+        self.checkBox_2.setGeometry(QtCore.QRect(610, 160, 92, 23))
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.pushButton_2 = QtWidgets.QPushButton(Widget)
+        self.pushButton_2.setGeometry(QtCore.QRect(300, 10, 281, 41))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.textEdit = QtWidgets.QTextEdit(Widget)
+        self.textEdit.setGeometry(QtCore.QRect(10, 160, 761, 191))
+        self.textEdit.setObjectName("textEdit")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(Widget)
+        self.plainTextEdit.setGeometry(QtCore.QRect(10, 410, 761, 221))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.label_2 = QtWidgets.QLabel(Widget)
+        self.label_2.setGeometry(QtCore.QRect(20, 370, 261, 17))
+        self.label_2.setObjectName("label_2")
+        self.pushButton_3 = QtWidgets.QPushButton(Widget)
+        self.pushButton_3.setGeometry(QtCore.QRect(600, 10, 171, 41))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(Widget)
+        self.pushButton_4.setGeometry(QtCore.QRect(720, 640, 51, 25))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(Widget)
+        self.pushButton_5.setGeometry(QtCore.QRect(10, 60, 281, 41))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(Widget)
+        self.pushButton_6.setGeometry(QtCore.QRect(300, 60, 281, 41))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(Widget)
+        self.pushButton_7.setGeometry(QtCore.QRect(640, 640, 71, 25))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.label_3 = QtWidgets.QLabel(Widget)
+        self.label_3.setGeometry(QtCore.QRect(310, 630, 161, 51))
+        self.label_3.setObjectName("label_3")
+
+        self.retranslateUi(Widget)
+        QtCore.QMetaObject.connectSlotsByName(Widget)
+
+    def retranslateUi(self, Widget):
+        _translate = QtCore.QCoreApplication.translate
+        Widget.setWindowTitle(_translate("Widget", "Переставщик"))
+        self.label.setText(_translate("Widget", "Входные данные"))
+        self.pushButton.setText(_translate("Widget", "Преобразовать на русскую раскладку"))
+        self.checkBox_2.setText(_translate("Widget", "CheckBox"))
+        self.pushButton_2.setText(_translate("Widget", "Перевести на английскую раскладку"))
+        self.label_2.setText(_translate("Widget", "Результат обработки"))
+        self.pushButton_3.setText(_translate("Widget", "Изменить регистер"))
+        self.pushButton_4.setText(_translate("Widget", "?"))
+        self.pushButton_5.setText(_translate("Widget", "Инверсия"))
+        self.pushButton_6.setText(_translate("Widget", "Инверсия с Caps Lock"))
+        self.pushButton_7.setText(_translate("Widget", "связь"))
+        self.label_3.setText(_translate("Widget", "© Иван КоZлоV"))
